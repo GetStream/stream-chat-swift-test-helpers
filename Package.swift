18172d7134a84042718e2d619529abe1a151230b
Boris Bielik <h3sperian@gmail.com>
2022-05-20 15:46:18 +0200

    New extensions for json and string/substring

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "StreamChatTestHelpers",
+    platforms: [
+        .iOS(.v11), .macOS(.v10_15)
+    ],
     products: [
         .library(
             name: "StreamChatTestHelpers",

b7a09e53a6647df94c02a67770ec876faa707f9d
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2023-11-20 15:07:48 +0000

    Remove Difference

--- Package.swift
+++ Package.swift
@@ -15,15 +15,20 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "Difference", url: "https://github.com/krzysztofzablocki/Difference.git", .exact("1.0.1")),
-        .package(name: "SnapshotTesting", url: "https://github.com/pointfreeco/swift-snapshot-testing", .exact("1.11.1"))
+        .package(
+            name: "SnapshotTesting",
+            url: "https://github.com/pointfreeco/swift-snapshot-testing",
+            .exact("1.11.1")
+        )
     ],
     targets: [
         .target(
             name: "StreamSwiftTestHelpers",
             dependencies: [
-                .product(name: "Difference", package: "Difference"),
-                .product(name: "SnapshotTesting", package: "SnapshotTesting")
+                .product(
+                    name: "SnapshotTesting",
+                    package: "SnapshotTesting"
+                )
             ],
             path: "Sources/TestHelpers"
         )
